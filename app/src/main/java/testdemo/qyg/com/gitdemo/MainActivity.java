80615dffde640b3b544362a7eb491403810d2119
屈云国 <qyg712314979@163.com>
2018-06-27 11:06:28 +0800

    文件提交

--- app/src/main/java/testdemo/qyg/com/gitdemo/MainActivity.java
+++ app/src/main/java/testdemo/qyg/com/gitdemo/MainActivity.java
@@ -1,13 +1,31 @@
 package testdemo.qyg.com.gitdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
+        initView();
+        initEvent();
+    }
+
+    private void initData() {
+
+    }
+
+    private void initView() {
+        tv_test = (TextView) findViewById(R.id.tv_test);
+        tv_test.setText("hello world !");
+    }
+
+    private void initEvent() {
+
     }
 }
